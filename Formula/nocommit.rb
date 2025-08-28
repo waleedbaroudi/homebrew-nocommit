4f72ba856d304661ac4e74465badc98ed49f94b7
Walid Baroudi <wbaroudi@carex.ai>
2025-08-29 00:31:51 +0300

    Release 0.4.0

--- Formula/nocommit.rb
+++ Formula/nocommit.rb
@@ -1,15 +1,25 @@
 class Nocommit < Formula
   desc "Prevent accidental commits with forbidden phrases"
   homepage "https://github.com/waleedbaroudi/nocommit"
-  version "0.1.1"
+  version "0.4.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/waleedbaroudi/nocommit/releases/download/v0.1.1/nocommit_darwin_arm64.tar.gz"
-      sha256 "4d53cd18f813bfd498e71aeca22bc770c4e2d1192518c297a41e27ce32736f65"
+      url "https://github.com/waleedbaroudi/nocommit/releases/download/#{version}/nocommit_darwin_arm64.tar.gz"
+      sha256 "d82985c8f99a5ed62bffe9e06d15c1fb1389966ae47d483fbc693f5a43624168"
     else
-      url "https://github.com/waleedbaroudi/nocommit/releases/download/v0.1.1/nocommit_darwin_amd64.tar.gz"
-      sha256 "7a4228bc9ce815f5b152d9921e2bceddca9e43e44d7642b4ba3d9a0f9896a724"
+      url "https://github.com/waleedbaroudi/nocommit/releases/download/#{version}/nocommit_darwin_amd64.tar.gz"
+      sha256 "1f0307716e8ca4ce58c89062916e6c02b3b105ac8b580c266b6ae84d1a3a2e01"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/waleedbaroudi/nocommit/releases/download/#{version}/nocommit_linux_arm64.tar.gz"
+      sha256 "f61b39ebdfcc7d8833e87827a147f9eb962a0416fdc6536687a06a17c454eb86"
+    else
+      url "https://github.com/waleedbaroudi/nocommit/releases/download/#{version}/nocommit_linux_amd64.tar.gz"
+      sha256 "6b2afda07ad54921f0390393e79417e70f4cedac7ad6a150e8753f83d617798e"
     end
   end
 
